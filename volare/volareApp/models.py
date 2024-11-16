9b1bdb2c205aaed9287c2e94212e9ef684441d78
NaiaLorente <naia.lorente@opendeusto.es>
2024-11-16 09:39:18 +0100

    las banderas se cogen de la bd

--- volare/volareApp/models.py
+++ volare/volareApp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Pais(models.Model):
     nombre = models.CharField(max_length=50)
     capital = models.CharField(max_length=100, default="No especificado")
