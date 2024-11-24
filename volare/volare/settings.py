1d475834e0b83a1adf380a1ba495483c35907722
Unai Olaizola <unai.olaizola.osa@opendeusto.es>
2024-11-24 18:20:32 +0100

    Prueba deploy en Render

--- volare/volare/settings.py
+++ volare/volare/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8w+t=nbn%o5hhms-rd3j%5hu!a&b5=hqo_(6(gmw&qfway&sg^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-8w+t=nbn%o5hhms-rd3j%5hu!a&b5=hqo_(6(gmw&qfway&sg^')
+
+# Nuestra secret key de default: 'django-insecure-8w+t=nbn%o5hhms-rd3j%5hu!a&b5=hqo_(6(gmw&qfway&sg^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['volare.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'volare.urls'
@@ -117,6 +120,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "volareApp/static",
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -150,3 +155,6 @@ LANGUAGES = [
 
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  
+
+# Parte de base de datos para ser configurado en render
+import dj_database_url
