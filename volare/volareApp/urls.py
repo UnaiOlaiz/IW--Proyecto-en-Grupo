441f64ad571816ef13c834cbd91e205f27074b26
elbabarrueta <144673239+elbabarrueta@users.noreply.github.com>
2024-11-30 13:07:50 +0100

    Uso de vistas basadas en clases en todas las funcionalidades básicas

--- volare/volareApp/urls.py
+++ volare/volareApp/urls.py
@@ -3,24 +3,47 @@ from . import views
 from .views import AerolineaCreateView, PaisCreateView, AeropuertoCreateView
 
 
-urlpatterns = [
-    path('', views.index, name='index'),    
-    path('paises/', views.index_Paises, name='index_paises'),   
-    path('paises/<int:pais_id>/', views.show_Pais, name='show_pais'),   
-    path('aeropuertos/', views.index_Aeropuerto, name='index_aeropuerto'),     
-    path('aeropuertos/<int:aeropuerto_id>/', views.show_Aeropuerto, name='show_aeropuerto'),    
-    path('aerolineas/', views.index_Aerolineas, name='index_aerolineas'),   
-    path('aerolineas/<int:aerolinea_id>/', views.show_Aerolinea, name='show_aerolinea'),
+#urlpatterns = [
+#    path('', views.index, name='index'),    
+#    path('paises/', views.index_Paises, name='index_paises'),   
+#    path('paises/<int:pais_id>/', views.show_Pais, name='show_pais'),   
+#    path('aeropuertos/', views.index_Aeropuerto, name='index_aeropuerto'),     
+#    path('aeropuertos/<int:aeropuerto_id>/', views.show_Aeropuerto, name='show_aeropuerto'),    
+#    path('aerolineas/', views.index_Aerolineas, name='index_aerolineas'),   
+#    path('aerolineas/<int:aerolinea_id>/', views.show_Aerolinea, name='show_aerolinea'),
 
-    path('aerolineas/nueva/', AerolineaCreateView.as_view(), name='formulario_aerolinea'),
+#    path('aerolineas/nueva/', AerolineaCreateView.as_view(), name='formulario_aerolinea'),
 
-    path('paises/nuevo/', PaisCreateView.as_view(), name='formulario_pais'),
-    path('aeropuertos/nuevo/', AeropuertoCreateView.as_view(), name='formulario_aeropuerto'),   
+#    path('paises/nuevo/', PaisCreateView.as_view(), name='formulario_pais'),
+#    path('aeropuertos/nuevo/', AeropuertoCreateView.as_view(), name='formulario_aeropuerto'),   
     
-    path('formulario/lista/', views.lista_formularios, name='lista_formularios'),
+#    path('formulario/lista/', views.lista_formularios, name='lista_formularios'),
 
     # Vue
+#    path('vue/', views.vue_spa, name="vue_spa"),
+#]
+
+
+
+from django.urls import path
+from .views import IndexView, PaisListView, PaisDetailView, AeropuertoListView, AeropuertoDetailView, AerolineaListView, AerolineaDetailView
+from .views import PaisCreateView, AeropuertoCreateView, AerolineaCreateView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),    
+    path('paises/', PaisListView.as_view(), name='index_paises'),  
+    path('paises/<int:pk>/', PaisDetailView.as_view(), name='show_pais'),   
+    path('aeropuertos/', AeropuertoListView.as_view(), name='index_aeropuerto'),  
+    path('aeropuertos/<int:pk>/', AeropuertoDetailView.as_view(), name='show_aeropuerto'),  
+    path('aerolineas/', AerolineaListView.as_view(), name='index_aerolineas'),   
+    path('aerolineas/<int:pk>/', AerolineaDetailView.as_view(), name='show_aerolinea'),
+    path('paises/nuevo/', PaisCreateView.as_view(), name='formulario_pais'),
+    path('aeropuertos/nuevo/', AeropuertoCreateView.as_view(), name='formulario_aeropuerto'),   
+    path('aerolineas/nueva/', AerolineaCreateView.as_view(), name='formulario_aerolinea'),
+
+    # Página de lista de formularios (sin cambios)
+    path('formulario/lista/', views.lista_formularios, name='lista_formularios'),
+
+    # Vue SPA (sin cambios)
     path('vue/', views.vue_spa, name="vue_spa"),
 ]
-
-
--- volare/volareApp/views.py
+++ volare/volareApp/views.py
@@ -1,62 +1,88 @@
 from django.shortcuts import get_object_or_404, get_list_or_404, render
 from .models import Pais, Aerolinea, Aeropuerto
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .forms import PaisForm, AeropuertoForm, AerolineaForm
 
 # obtiene aerolinea y pais
-def index(request):
+#def index(request):
     # Obtener todos los países
-    paises = get_list_or_404(Pais.objects.order_by('nombre'))
+#    paises = get_list_or_404(Pais.objects.order_by('nombre'))
     # País y su aerolínea más antigua
-    pais_aerolineas = {}
+#    pais_aerolineas = {}
 
-    for pais in paises:
+#    for pais in paises:
         # Obtener la aerolínea más antigua de cada país
-        aerolinea_antigua = pais.aerolinea_set.order_by('fundacion').first()
-        pais_aerolineas[pais] = aerolinea_antigua
+#        aerolinea_antigua = pais.aerolinea_set.order_by('fundacion').first()
+#        pais_aerolineas[pais] = aerolinea_antigua
 
-    context = {'pais_aerolineas': pais_aerolineas}
-    return render(request, 'index.html', context)
+#    context = {'pais_aerolineas': pais_aerolineas}
+#    return render(request, 'index.html', context)
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        # Llama al contexto original del TemplateView
+        context = super().get_context_data(**kwargs)
+
+        # Obtener todos los países
+        paises = Pais.objects.order_by('nombre')
+        
+        # Crear un diccionario para guardar los países y su aerolínea más antigua
+        pais_aerolineas = {}
+
+        for pais in paises:
+            # Obtener la aerolínea más antigua de cada país
+            aerolinea_antigua = pais.aerolinea_set.order_by('fundacion').first()
+            pais_aerolineas[pais] = aerolinea_antigua
+
+        # Añadir el diccionario de pais_aerolineas al contexto
+        context['pais_aerolineas'] = pais_aerolineas
+        return context
 
 # lista de paises
-def index_Paises(request):
-    paises = get_list_or_404(Pais.objects.order_by('nombre'))
-    context = {'lista_paises': paises}
-    return render(request, 'lista_pais.html', context)
+class PaisListView(ListView):
+    model = Pais
+    template_name = 'lista_pais.html'
+    context_object_name = 'lista_paises'
+    queryset = Pais.objects.order_by('nombre')
 
-# detalles de pais usando ID
-def show_Pais(request, pais_id):
-    pais = get_object_or_404(Pais, pk=pais_id)
-    context = {'pais': pais}
-    return render(request, 'country_detail.html', context)
+# detalles de pais 
+class PaisDetailView(DetailView):
+    model = Pais
+    template_name = 'country_detail.html'
+    context_object_name = 'pais'
 
 # lista de todos los aeropuertos
-def index_Aeropuerto(request):
-    aeropuertos = get_list_or_404(Aeropuerto.objects.order_by('nombre'))
-    context = {'lista_aeropuertos': aeropuertos}
-    return render(request, 'lista_airport.html', context)
+class AeropuertoListView(ListView):
+    model = Aeropuerto
+    template_name = 'lista_airport.html'
+    context_object_name = 'lista_aeropuertos'
+    queryset = Aeropuerto.objects.order_by('nombre')
 
-# detalles de aeropuerto usando ID
-def show_Aeropuerto(request, aeropuerto_id):
-    aeropuerto = get_object_or_404(Aeropuerto, pk=aeropuerto_id)
-    context = {'aeropuerto': aeropuerto}
-    return render(request, 'airport_detail.html', context)
+# detalles de aeropuerto 
+class AeropuertoDetailView(DetailView):
+    model = Aeropuerto
+    template_name = 'airport_detail.html'
+    context_object_name = 'aeropuerto'
 
 # lista aerolineas
-def index_Aerolineas(request):
-    aerolineas = get_list_or_404(Aerolinea.objects.order_by('nombre'))
-    context = {'lista_aerolineas': aerolineas}
-    return render(request, 'lista_airline.html', context)
+class AerolineaListView(ListView):
+    model = Aerolinea
+    template_name = 'lista_airline.html'
+    context_object_name = 'lista_aerolineas'
+    queryset = Aerolinea.objects.order_by('nombre')
 
-# detalles de aerolinea usando ID
-def show_Aerolinea(request, aerolinea_id):
-    aerolinea = get_object_or_404(Aerolinea, pk=aerolinea_id)  # Obtener la aerolínea
-    context = {
-        'airline': aerolinea,
-        'airports': aerolinea.aeropuertos.all()  # Obtener aeropuertos relacionados
-    }
-    return render(request, 'airline_detail.html', context)
+# detalles de aerolinea 
+class AerolineaDetailView(DetailView):
+    model = Aerolinea
+    template_name = 'airline_detail.html'
+    context_object_name = 'airline'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['airports'] = self.object.aeropuertos.all()  # Relación de aeropuertos de la aerolínea
+        return context
 
 class PaisCreateView(CreateView):
     model = Pais
